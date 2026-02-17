7919445f19d70ea26cec67b6371ccb5b7f2b875d
ShewinVega <edwinvega.aid@gmail.com>
2026-02-16 20:18:20 -0600

    refactor: Description refactored, and hope Api status description changed

--- __mocks__/home.ts
+++ __mocks__/home.ts
@@ -7,12 +7,12 @@ export const profile = {
     Desarrolador FullStack innovador con sede en Nicaragua.  Mi objetivo es crear soluciones claras, útiles y escalables que impulsen el crecimiento empresarial.
   `,
   description_es: `Desarrollador frontend y full-stack con más de tres años de experiencia en proyectos freelance,
-  especializado en tecnologías modernas como React, Node.js, NestJS, Express, Svelte y GraphQL.
+  especializado en tecnologías modernas como React, .Net core, Node.js, Express, Svelte y GraphQL.
   He participado en el desarrollo de sitios web completos y en la construcción de APIs robustas, además de mantener proyectos personales que impulsan mi aprendizaje continuo.
   Me destaco por mi capacidad para comunicarme efectivamente, resolver problemas con creatividad y adaptarme rápidamente a nuevas herramientas y desafíos.
   Estoy en búsqueda de un rol donde pueda aportar mis habilidades técnicas y, al mismo tiempo, seguir creciendo profesionalmente en un ambiente dinámico y colaborativo.`,
   description_en: `
-  Frontend and full-stack developer with over three years of experience in freelance projects, specializing in modern technologies such as React, Node.js, NestJS, Express, Svelte, and GraphQL.
+  Frontend and full-stack developer with over three years of experience in freelance projects, specializing in modern technologies such as React, .Net core, Node.js, Express, Svelte, and GraphQL.
   I have contributed to the development of complete websites and built robust APIs, alongside maintaining personal projects that drive my continuous learning.
   I stand out for my effective communication skills, creative problem-solving, and ability to quickly adapt to new tools and challenges.
   I am seeking a role where I can contribute my technical skills while continuing to grow professionally in a dynamic and collaborative environment.
@@ -25,9 +25,9 @@ export const profile = {
 
     <br />
     <br />
-    Me he especializado en tecnologías modernas como React, Svelte, Astro, Node.js, NestJS, Express y GraphQL.
+    Me he especializado en tecnologías modernas como React, Svelte, Astro, .Net core, Node.js, Express y GraphQL.
     Actualmente estoy <span class="text-primary">colaborando en el desarrollo de una app orientada al aprendizaje para niños con TEA</span>,
-    la cual forma parte de un trabajo de investigación aplicado que estoy llevando a cabo.
+    la cual forma parte de un trabajo de investigación aplicado que estoy llevando a cabo, en donde ya esta en su etapa final de publicación.
     <br />
     <br />
     Me destaco por mi capacidad para <span class="text-primary">comunicarme efectivamente, resolver problemas con creatividad y adaptarme rápidamente a nuevas herramientas y desafíos.</span>
