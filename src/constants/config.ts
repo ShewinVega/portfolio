e0f05a87494f2a00e78c2176ae0e1dbc08728c93
ShewinVega <edwinvega.aid@gmail.com>
2026-03-15 20:01:05 -0600

    feat: constants created, icons added and color styles added too.

---
+++ src/constants/config.ts
@@ -0,0 +1,13 @@
+
+
+
+const tailwindBreakpoints = {
+    sm: '640px',
+    md: '768px',
+    lg: '1024px',
+    xl: '1280px',
+} as const;
+
+export const CONFIG = {
+    BREAKPOINTS: tailwindBreakpoints,
+};
